99f9d3530249f5333b77a198ab2b840eb556d787
jc-ssymonov <ssymonov@justcoded.com>
2025-09-20 21:23:40 +0300

    fix: added .gitignore

--- cf7-custom-validation.php
+++ cf7-custom-validation.php
@@ -5,7 +5,7 @@
  * Version: 1.1.0
  * Requires at least: 5.8
  * Requires PHP: 7.4
- * Author: ssymonov
+ * Author: simmotorlp
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: cf7-custom-validation
